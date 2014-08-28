4626d5492e579f81f766dfe03fe6a9757135476b
Carlos Luis <carlosluis_lopez@yahoo.com>
2014-08-28 00:11:31 -0600

    Proyecto DataLogic
    Se agrego el proyecto DataLogic que va servir de interfaz entre los
    controllers de MVC y la Entidades

---
+++ DoctorFlow/DoctorFlow.DataLogic/UserAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+//using DoctorFlow.Entities;
+//using DoctorFlow.Entities.Context;
+//using DoctorFlow.Entities.Models;
+
+namespace DoctorFlow.DataLogic
+{
+    public class UserAccount
+    {
+        public bool CreateUser(string UserName, string Password, string Name, string LastName, string Email)
+        {
+            return false;
+        }
+
+        public bool Login(string UserNameEmail, string Password)
+        {
+            return false;
+        }
+    }
+}
